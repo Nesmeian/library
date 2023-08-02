90bde2024096518e1bac41a584da22266b4418e9
leonid Kozlovskiy <lenya.kozlovskiy.97@mail.ru>
2023-08-02 17:29:42 +0300

    feat:add burger

--- js/index.js
+++ js/index.js
@@ -2,4 +2,26 @@ console.log(`1.Вёрстка валидная +10 \n
 2.Вёрстка семантическая +16\n
 3.Вёрстка соответствует макету +54 \n
 4.Общие требования к верстке +20\n
-Score:100/100`)
+Score:100/100`);
+
+//!Burger-menu
+const burger = document.querySelector(".header__burger_meny");
+const nav = document.querySelector(".nav__list");
+const navActive = document.querySelector(".active");
+burger.addEventListener("click", () => {
+  burger.classList.toggle("active");
+  nav.classList.toggle("active");
+});
+
+document.querySelectorAll(".nav__list").forEach((e) =>
+  e.addEventListener("click", () => {
+    burger.classList.remove("active");
+    nav.classList.remove("active");
+  })
+);
+document.addEventListener("mouseup", (e) => {
+  if (e.target !== navActive) {
+    burger.classList.remove("active");
+    nav.classList.remove("active");
+  }
+});
