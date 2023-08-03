be0aabe6c87913590db5980babfd699f04f1e53e
leonid Kozlovskiy <lenya.kozlovskiy.97@mail.ru>
2023-08-03 20:52:49 +0300

    feat:fix library text

--- js/index.js
+++ js/index.js
@@ -8,23 +8,26 @@ Score:100/100`);
 const burger = document.querySelector(".header__burger_meny");
 const nav = document.querySelector(".nav__list");
 const navActive = document.querySelector(".active");
+const logo = document.querySelector(".logo");
 const wrapper = document.querySelector(".wrapper");
 burger.addEventListener("click", (e) => {
   burger.classList.toggle("active");
   nav.classList.toggle("active");
+  logo.classList.toggle("logo--active");
 });
 
 document.querySelectorAll(".nav__list").forEach((e) =>
   e.addEventListener("click", () => {
     burger.classList.remove("active");
     nav.classList.remove("active");
+    logo.classList.remove("logo--active");
   })
 );
 
 document.addEventListener("click", (e) => {
   if (!wrapper.contains(e.target)) {
     nav.classList.remove("active");
-    console.log(1);
+    burger.classList.remove("active");
   }
 });
 
