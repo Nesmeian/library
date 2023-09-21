89f7418aebf79441cd94844cf6c96c2c3a43d499
leonid Kozlovskiy <lenya.kozlovskiy.97@mail.ru>
2023-09-21 18:07:11 +0300

    feat: add songDurring,progress bar,time length

--- audio-player/index.js
+++ audio-player/index.js
@@ -5,6 +5,10 @@ const btnPlay = document.querySelector(".button-play");
 const karusel = document.querySelector(".audio-player__image");
 const songAuthor = document.querySelector(".song__author");
 const songName = document.querySelector(".song__name");
+const progressBarDurring = document.querySelector(".progress__bar--during");
+const lengthOfSong = document.querySelector(".time-length");
+const progressBar = document.querySelector(".progress__bar");
+const currentSongTime = document.querySelector(".current-time");
 let isPlay = false;
 let currentTreck = 0;
 let position = 0;
@@ -54,6 +58,8 @@ function toggleBtn() {
     stopAudio();
   }
 }
+
+btnPlay.addEventListener("click", toggleBtn);
 btnPrev.addEventListener("click", () => {
   stopAudio();
   currentTreck--;
@@ -70,5 +76,34 @@ btnNext.addEventListener("click", () => {
   checkPicture();
   playAudio();
 });
-btnPlay.addEventListener("click", toggleBtn);
-console.log(audio.length);
+
+console.log(progressBar);
+progressBar.addEventListener("click", (e) => {
+  const timelineWidth = window.getComputedStyle(progressBar).width;
+  const timeToSeek =
+    (e.offsetX / parseInt(timelineWidth)) * audio[currentTreck].duration;
+  audio[currentTreck].currentTime = timeToSeek;
+});
+
+setInterval(() => {
+  lengthOfSong.textContent = getTimeCodeFromNum(audio[currentTreck].duration);
+  progressBarDurring.style.width =
+    (audio[currentTreck].currentTime / audio[currentTreck].duration) * 100 +
+    "%";
+  currentSongTime.textContent = getTimeCodeFromNum(
+    audio[currentTreck].currentTime
+  );
+});
+
+function getTimeCodeFromNum(num) {
+  let seconds = parseInt(num);
+  let minutes = parseInt(seconds / 60);
+  seconds -= minutes * 60;
+  const hours = parseInt(minutes / 60);
+  minutes -= hours * 60;
+
+  if (hours === 0) return `${minutes}:${String(seconds % 60).padStart(2, 0)}`;
+  return `${String(hours).padStart(2, 0)}:${minutes}:${String(
+    seconds % 60
+  ).padStart(2, 0)}`;
+}
