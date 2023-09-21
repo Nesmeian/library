d7a101d6ba0238f1d58078300a5c0ca411a4b535
leonid Kozlovskiy <lenya.kozlovskiy.97@mail.ru>
2023-09-21 15:24:56 +0300

    feat:add change song name

--- audio-player/index.js
+++ audio-player/index.js
@@ -3,6 +3,8 @@ const btnPrev = document.querySelector(".prev");
 const btnNext = document.querySelector(".next");
 const btnPlay = document.querySelector(".button-play");
 const karusel = document.querySelector(".audio-player__image");
+const songAuthor = document.querySelector(".song__author");
+const songName = document.querySelector(".song__name");
 let isPlay = false;
 let currentTreck = 0;
 let position = 0;
@@ -25,10 +27,16 @@ function checkCurent() {
 
 function checkPicture() {
   if (currentTreck == 0) {
+    songAuthor.innerHTML = "King and Joker";
+    songName.innerHTML = "Man was eating meat";
     karusel.style.transform = `translate(0%)`;
   } else if (currentTreck == 1) {
+    songAuthor.innerHTML = "GoodMask";
+    songName.innerHTML = "BULLETPROOF";
     karusel.style.transform = `translate(-25%)`;
   } else if (currentTreck == 2) {
+    songAuthor.innerHTML = "Evans Bluse";
+    songName.innerHTML = "This Time it's Different";
     karusel.style.transform = `translate(-50%)`;
   }
 }
