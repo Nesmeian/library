0a404dd87cbc6c2c8890a05e587432528a6adbf2
leonid Kozlovskiy <lenya.kozlovskiy.97@mail.ru>
2023-09-26 18:43:10 +0300

    feat:add loyalt

---
+++ image-galery/index.js
@@ -0,0 +1,9 @@
+// const url =
+//   "https://api.themoviedb.org/3/search/movie?api_key=3fd2be6f0c70a2a598f084ddfb75487c&query=spring";
+
+// async function getData() {
+//   const res = await fetch(url);
+//   const data = await res.json();
+//   console.log(data);
+// }
+// getData();
